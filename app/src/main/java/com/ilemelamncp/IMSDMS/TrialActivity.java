83e8ee32b6e7852d31709ed980f0e506b98341e0
Magic Mike XXL <sebastianernest5499@gmail.com>
2020-07-28 20:08:32 +0300

    Add Validation

--- app/src/main/java/com/ilemelamncp/IMSDMS/TrialActivity.java
+++ app/src/main/java/com/ilemelamncp/IMSDMS/TrialActivity.java
@@ -1,16 +1,61 @@
 package com.ilemelamncp.IMSDMS;
 
+import android.content.*;
 import android.os.*;
 import android.support.v7.app.*;
+import android.view.*;
 import android.widget.*;
 
 public class TrialActivity extends AppCompatActivity
 {
+	
+	private EditText inputName, inputEmail, inputPass, inputRepass;
+	
 	@Override
 	protected void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
 		setContentView(R.layout.trial);
-		EditText inputName = findViewById(R.id.et_name);
-		EditText inputEmail = findViewById(R.id.et_email);
+		inputName = findViewById(R.id.et_name);
+		inputEmail = findViewById(R.id.et_email);
+		inputPass = findViewById(R.id.et_password);
+		inputRepass = findViewById(R.id.et_repassword);
+		Button register = findViewById(R.id.btn_register);
+		register.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View p1)
+				{
+					if (infoIsValid()) {
+						startActivity(new Intent(TrialActivity.this, MainActivity.class));
+					}
+				}
+		});
+	}
+	
+	private boolean infoIsValid() {
+		String name = inputName.getText().toString();
+		String email = inputEmail.getText().toString();
+		String password = inputPass.getText().toString();
+		String repass = inputRepass.getText().toString();
+		if (name.isEmpty()) {
+			inputName.setError("Name Cannot Be Empty");
+			return false;
+		}
+		if (email.isEmpty()) {
+			inputEmail.setError("Email Cannot Be Empty");
+			return false;
+		}
+		if (password.isEmpty()) {
+			inputPass.setError("Password Cannot Be Empty");
+			return false;
+		}
+		if (repass.isEmpty()) {
+			inputRepass.setError("Password Cannot Be Empty");
+			return false;
+		}
+		if (!inputRepass.equals(password)) {
+			inputRepass.setError("Passwords Do Not Match");
+			return false;
+		}
+		return true;
 	}
 }
